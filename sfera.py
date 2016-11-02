7659260ed9ca1551de57902a2bef9f10ec0b55e2
CarloRuzzon <carloruzzon@gmail.com>
2016-11-02 11:28:37 +0100

    Risoluzione problema

--- sfera.py
+++ sfera.py
@@ -1,8 +1,8 @@
 #sfera
 import math
-r=input("Inserire il raggio: ")
+r = input("Inserire il raggio: ")
 areas= 4 * math.pi * r * r
-volume= 4/3 * math.pi * r * r * r
+volume=  4.0/3.0 * math.pi * r * r * r
 print "L'area superficiale e'", areas
 print "Il volume e'", volume
 
