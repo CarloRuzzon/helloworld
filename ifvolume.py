f7ea85136d6246a4b4ef0843bb2fc251fdd0f9ce
CarloRuzzon <carloruzzon@gmail.com>
2016-11-02 20:03:05 +0100

    If volume

---
+++ ifvolume.py
@@ -0,0 +1,14 @@
+#sfera o cubo?
+import math
+scelta=input("Premere 1 per scegliere il cubo e 2 per la sfera: ")
+if scelta == 1:
+
+  lato=input("Inserire il lato del cubo: ")
+  volumel= lato * lato *lato
+  print "Il volume del cubo e'", volumel
+
+elif scelta==2:
+
+  raggio=input("Inserire il raggio della sfera: ")
+  volumes= 4.0/3.0 * math.pi * r * r
+  print "Il volume della sfera e'", volumes
