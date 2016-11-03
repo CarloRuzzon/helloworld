91d7ddc23ae7c57865a328d0e83d2c09b0c1a02d
CarloRuzzon <carloruzzon@gmail.com>
2016-11-03 16:46:30 +0100

    secondo grado

--- secgrado.py
+++ secgrado.py
@@ -12,11 +12,11 @@ elif a==0 and b==0:
   print "0 =", c
 elif a==0 and c==0 or b==0 and c==0:
   print "x = 0" 
-else
+else:
   s = math.pow(b,2) - 4 * a * c
   if s<=0:
     print "L'equazione e' inpossibile per delta minore o uguale a 0"
-  else
+  else:
     sol1=-(float(b)+math.sqrt(s))/2.0
     sol2=-(float(b)-math.sqrt(s))/2.0
     print "x1 =", sol1
