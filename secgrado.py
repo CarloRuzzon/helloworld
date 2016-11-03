09d3e791971a8c267ca2f02a4ed11294fdd7b77b
CarloRuzzon <carloruzzon@gmail.com>
2016-11-03 16:45:24 +0100

    secondo grado

--- secgrado.py
+++ secgrado.py
@@ -6,19 +6,18 @@ c=input("Inserire il termine noto: ")
 s = math.pow(b,2) - 4 * a * c
 #calcolo delta
 
-if b==0 and c==0:
-  print "x = 0"
-elif b==0:
-  if c>0:
-    print "x = +- radice quadrata di", -float(c)/float(a)
-  elif c<0:
-    print "x = +-", -float(c)/float(a)
-elif c==0:
-  print "l'equazione e' spuria" 
-elif s<=0:
-  print "L'equazione e' impossibile per delta minore o uguale a 0"
-else:
-  sol1=-(float(b)+math.sqrt(s))/2.0
-  sol2=-(float(b)-math.sqrt(s))/2.0
-  print "x1 =", sol1
-  print "x2 =", sol2
+if a==0 and b==0 and c==0:
+  print "L'eq e' 0"
+elif a==0 and b==0:
+  print "0 =", c
+elif a==0 and c==0 or b==0 and c==0:
+  print "x = 0" 
+else
+  s = math.pow(b,2) - 4 * a * c
+  if s<=0:
+    print "L'equazione e' inpossibile per delta minore o uguale a 0"
+  else
+    sol1=-(float(b)+math.sqrt(s))/2.0
+    sol2=-(float(b)-math.sqrt(s))/2.0
+    print "x1 =", sol1
+    print "x2 =", sol2
